4ab63b3006709d9a0e5e24857db0c2c54859190b
Reagan Cline <reaganecline@gmail.com>
2018-06-30 21:21:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
-//define functions here
+// Define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+  return; 
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+  return;
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which==71){
+      alert('You pressed G')
+    }
+  });
+  return;
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  });
+  return;
+}
 
 $(document).ready(function(){
-
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
+
